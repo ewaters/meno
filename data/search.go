45fa727595d82c712baac0e29cbac10db5fc34b9
Eric Waters <eric@novalingvo.app>
2023-08-23 09:44:47 +0200

    Channels-based searching
    Tests currently fail as I'm not handling the newline character properly

--- data/search.go
+++ data/search.go
@@ -74,3 +74,69 @@ func (p *runningSearch) run() {
 		finished: true,
 	}
 }
+
+type SearchRequest struct {
+	Query         string
+	ResultC       chan searchResult
+	QuitC         chan struct{}
+	MaxResults    int
+	SearchUp      bool
+	StartFromLine int
+	Logf          func(string, ...interface{})
+}
+
+func NewSearchRequest(query string) SearchRequest {
+	return SearchRequest{
+		Query:         query,
+		ResultC:       make(chan searchResult),
+		QuitC:         make(chan struct{}),
+		MaxResults:    1,
+		SearchUp:      false,
+		StartFromLine: 0,
+		Logf:          func(string, ...interface{}) {},
+	}
+}
+
+func (id *IndexedData) Search(req SearchRequest) {
+	returned, max := 0, req.MaxResults
+
+	keepGoing := func(i int) bool {
+		select {
+		case <-req.QuitC:
+			return false
+		default:
+		}
+
+		if !id.LineMatches(i, req.Query) {
+			return true
+		}
+
+		req.ResultC <- searchResult{
+			query:      req.Query,
+			lineNumber: i,
+		}
+		returned++
+		if max > 0 && returned >= max {
+			return false
+		}
+		return true
+	}
+
+	if req.SearchUp {
+		req.Logf("searching up from %d to 0", req.StartFromLine)
+		for i := req.StartFromLine; i >= 0; i-- {
+			if !keepGoing(i) {
+				break
+			}
+		}
+	} else {
+		req.Logf("searching down from %d to %d", req.StartFromLine, id.VisibleLines())
+		for i := req.StartFromLine; i < id.VisibleLines(); i++ {
+			if !keepGoing(i) {
+				break
+			}
+		}
+	}
+
+	close(req.ResultC)
+}
---
+++ data/search_test.go
@@ -0,0 +1,97 @@
+package data
+
+import (
+	"strings"
+	"testing"
+)
+
+func init() {
+	enableLogger = true
+}
+
+type searchTest struct {
+	query string
+	want  int
+}
+
+type basicTest struct {
+	width int
+	input string
+	want  []string
+}
+
+func (bt basicTest) indexedData(t *testing.T) *IndexedData {
+	t.Helper()
+	if bt.width == 0 {
+		bt.width = 15
+	}
+	r := strings.NewReader(bt.input)
+	id := NewIndexedData(r, bt.width)
+	var got []string
+	for _, vl := range id.lines {
+		line := vl.line
+		if vl.hasNewline {
+			line += "\n"
+		}
+		got = append(got, line)
+	}
+
+	if len(got) != len(bt.want) {
+		t.Logf("\n Got: %#v\nWant: %#v", got, bt.want)
+		t.Fatalf("Got %d lines, wanted %d", len(got), len(bt.want))
+	}
+	for i := range got {
+		if got[i] != bt.want[i] {
+			t.Fatalf("Line %d: got %q, want %q", i, got[i], bt.want[i])
+		}
+	}
+	return id
+}
+
+func TestSearch(t *testing.T) {
+	for _, tc := range []struct {
+		basicTest
+		tests []searchTest
+	}{
+		{
+			basicTest: basicTest{
+				input: "Now is the time\n for the good of our country",
+				want: []string{
+					"Now is the time\n",
+					" for the good o",
+					"f our country",
+				},
+			},
+			tests: []searchTest{
+				{"of", 1},
+				{"country", 2},
+				{"not present", -1},
+				{"the time\n for", 0},
+				{"the time for", -1},
+			},
+		},
+	} {
+		id := tc.indexedData(t)
+
+		for _, st := range tc.tests {
+			req := NewSearchRequest(st.query)
+			go id.Search(req)
+
+			var results []searchResult
+			for result := range req.ResultC {
+				results = append(results, result)
+			}
+			got := -1
+			if len(results) == 0 {
+				// Not found
+			} else if len(results) == 1 {
+				got = results[0].lineNumber
+			} else {
+				t.Fatalf("Got %d results, expected 1", len(results))
+			}
+			if got != st.want {
+				t.Fatalf("Query for %q found on line %d, expected %d", st.query, got, st.want)
+			}
+		}
+	}
+}
